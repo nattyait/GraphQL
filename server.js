39c6ae1874df21d627c21c83c7d95a31f2f2c3b8
nattyait <nattanicha@gmail.com>
2017-02-28 16:24:06 +0700

    Add Object type :baby:

--- server.js
+++ server.js
@@ -3,26 +3,35 @@ var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 
 var schema = buildSchema(`
+  type RandomDie {
+  	numSidesf: Int!
+  	rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  },
   type Query {
-   quoteOfTheDay: String
-   random: Float!
-   rollDice(numDice: Int!, numSides: Int): [Int]
+   getDie(numSides: Int): RandomDie
   },
 `);
 
+class RandomDie {
+    constructor(numSides){
+        this.numSides = numSides;
+    }
+    rollOnce() {
+        return 1 + Math.floor(Math.random() * this.numSides);
+    }
+    roll({numRolls}) {
+    	var output = [];
+    	for(var i = 0; i < numRolls; i++){
+    		output.push(this.rollOnce());
+    	}
+    	return output;
+    }
+}
+
 var root = {
-  quoteOfTheDay: () => {
-    return Math.random() < 0.5 ? 'Take it easy': 'Salvation lies within' ;
-  },
-  random: () => {
-  	return Math.random();
-  },
-  rollDice: function({numDice, numSides}) {
-  	var output = [];
-  	for (var i = 0; i < numDice; i++) {
-  		output.push(1 + Math.floor(Math.random() * (numSides || 6)));
-  	}
-  	return output;
+  getDie: function({numSides}) {
+    return new RandomDie(numSides || 6);
   },
 };
 
@@ -34,3 +43,12 @@ app.use('/graphql', graphqlHTTP({
 }));
 app.listen(4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
+
+/*
+{ 
+  getDie(numSides: 3){
+    rollOnce
+    roll(numRolls: 3)
+  }
+}
+/*
